514b243d53f70d3861c179bd1a6a1b1c26e9a013
KweonMo Koo <mikekoo2010@gmail.com>
2019-03-19 16:32:28 -0700

    Add request handlers for updating actual reservation start and end times

--- server/reservation/controller.js
+++ server/reservation/controller.js
@@ -5,10 +5,32 @@ module.exports = {
     db.reserve(req.body, (err, id) => {
       if (err) {
         console.error(err);
-        res.status(400).end();
+        res.status(404).end();
       } else {
         console.log('MADE RESERVATION')
         res.status(201).json(id);
+      }
+    })
+  },
+  startReservation: (req, res) => {
+    db.startRes(req.body, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(404).end();
+      } else {
+        console.log('UPDATE RESERVATION START TIME');
+        res.status(202).end();
+      }
+    })
+  },
+  endReservation: (req, res) => {
+    db.endRes(req.body, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(404).end();
+      } else {
+        console.log('UPDATE RESERVATION END TIME');
+        res.status(202).end();
       }
     })
   }
