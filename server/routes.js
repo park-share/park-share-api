a90d44b1a03ff1f4d5dbb05336e28c1f37edefb0
KweonMo Koo <mikekoo2010@gmail.com>
2019-03-19 15:16:24 -0700

    Add reservation method

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('./controllers.js')
+const controller = require('./controllers.js');
+const resController = require('./reservation/controller.js');
 
-
+router
+  .route('/reservations/post')
+  .post(resController.makeReservation);
 router.post('/users', controller.users.post);
 
 
