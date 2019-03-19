f9f7d97c22aba0d7777ce4b66a16997c2d6b965f
Caroline Cange <carolinecang@gmail.com>
2019-03-18 20:08:13 -0700

    created the foreign col and connected to the db

--- server/controllers.js
+++ server/controllers.js
@@ -1,18 +1,17 @@
-// const db = require('../database/model.js');
+const db = require('../database/index.js');
 
 module.exports = {
   users: {
     post: (req, res) => {
       console.log('in post');
-
-      // const { text } = req.body;
-      // db.todos.post(text, (err, results) => {
-      //   if (err) {
-      //     res.status(404).send(err)
-      //   } else {
-      //     res.status(201).send(results)
-      //   }
-      // })
+      const { firstName, lastName, email,user_password,birthday,phone } = req.body;
+      db.users.post(firstName, lastName, email, user_password, birthday, phone, (err, results) => {
+        if (err) {
+          res.status(404).send(err)
+        } else {
+          res.status(201).send(results)
+        }
+      })
     }
 
   }
