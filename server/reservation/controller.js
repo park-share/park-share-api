4d6b1af8636e2247da9874e848694296d30bf5d5
KweonMo Koo <mikekoo2010@gmail.com>
2019-03-19 16:02:39 -0700

    Send row id after initial reservation insertion

--- server/reservation/controller.js
+++ server/reservation/controller.js
@@ -2,13 +2,13 @@ const db = require('../../database/models.js').reservations;
 
 module.exports = {
   makeReservation: (req, res) => {
-    db.reserve(req.body, (err, msg) => {
+    db.reserve(req.body, (err, id) => {
       if (err) {
         console.error(err);
         res.status(400).end();
       } else {
         console.log('MADE RESERVATION')
-        res.status(201).send(msg);
+        res.status(201).json(id);
       }
     })
   }
