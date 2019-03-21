7d3ac90f125fcbcd7de4f7e64c38c0c1b981fe3a
KweonMo Koo <mikekoo2010@gmail.com>
2019-03-20 20:38:58 -0700

    Add https protocol

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const parser = require('body-parser');
 const PORT = 3000;
+const SSLPORT = 3030;
 const path = require('path');
 const router = require('./routes');
 const cors = require('cors');
 const app = express();
+
+const https = require('https');
+const http = require('http');
 
 app.use(parser.json());
 app.use(parser.urlencoded({extended:false}));
@@ -12,4 +16,5 @@ app.use(parser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname + "/../park-share-ui/client/dist/")));
 app.use('/api',router);
 
-app.listen(PORT,console.log('listen to the port', PORT));
+http.createServer(app).listen(PORT, console.log('HTTP LISTENING TO', PORT));
+https.createServer(app).listen(SSLPORT, console.log('HTTPS LISTENING TO', SSLPORT));
