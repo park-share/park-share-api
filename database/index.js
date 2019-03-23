bc34acebf6a4a7d3cae843edb016e530e90bad41
Caroline Cange <carolinecang@gmail.com>
2019-03-23 12:48:21 -0700

    testing

--- database/index.js
+++ database/index.js
@@ -14,26 +14,3 @@ pool.connect()
 module.exports = pool;
 
 
-// const connection = new Sequelize("park_share", "haoyancang", "", {
-//   host: "localhost",
-//   dialect: "postgres",
-//   operatorAliases: false,
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// });
-
-// connection
-//   .authenticate()
-//   .then(() => {
-
-//     console.log('connected')
-//   })
-//   .catch(err =>{
-//     console.log('unable to connect to the postgres database', err);
-//   });
-
-// module.exports = connection;
