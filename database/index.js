d2ab07c088549a9167bca84188c145973fea89ed
Caroline Cange <carolinecang@gmail.com>
2019-04-03 13:01:02 -0700

    the authentication function works

--- database/index.js
+++ database/index.js
@@ -2,11 +2,15 @@ const { Pool } = require('pg');
 // const Sequelize = require("sequelize");
 
 const pool = new Pool({
-  host: '54.183.143.103',
-  port: 5432,
-  database: 'park_share',
-  user: 'postgres',
-  password: 'parkshare'
+  // host: '54.183.143.103',
+  // port: 5432,
+  // database: 'park_share',
+  // user: 'postgres',
+  // password: 'parkshare'
+  host: 'localhost',
+  user: 'haoyancang',
+  database: 'park_share'
+  
 });
 
 pool.connect() 
