d28c76504bec8a278976a2ecbb28dfa27779ea96
Caroline Cange <carolinecang@gmail.com>
2019-04-09 00:03:45 -0700

    added passport with oAuth for google account users login

---
+++ server/passportControllers.js
@@ -0,0 +1,22 @@
+const { getPassport } = require("./passportConfig.js");
+
+const passport = getPassport();
+
+module.exports = {
+  authenticateUser: passport.authenticate("google", {
+    scope: ["https://www.googleapis.com/auth/userinfo.profile"]
+  }),
+  authenticateUser2: passport.authenticate("google", {
+    failureRedirect: "/"
+  }),
+  giveUserSessionToken: (req, res) => {
+    req.session.token = req.user.token;
+    if (req.cookies.sendToHome) {
+      console.log("found send to home");
+      res.redirect("/");
+    } else {
+      res.redirect("/after-auth.html");
+    }
+  },
+  passport: passport
+};
